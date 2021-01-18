c46cc0371f52d2425a23f4a3be272ca86cdf459f
primayy <primayy@naver.com>
2021-01-18 17:22:49 +0900

    update

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "minimal mistakes 테마를 이용해 github.io 블로그 구축하기",
+        "excerpt":" ","categories": ["blogging"],
+        "tags": [],
+        "url": "https://primayy.github.io//blogging/hello_world/",
+        "teaser": null
+      }]
